ad032f2b966d3ca440025540847640379ec80d9a
lawrenceli <lawrenceli@YSL>
2020-10-06 19:40:29 +0900

    v0

---
+++ BatchRenameRegex.py
@@ -0,0 +1,31 @@
+# Rename all files in a folder by regular expression
+# Arguments:
+#   py BatchRenameRegex.py <pattern> <repl> <folder>
+# Exmaples:
+#   py BatchRenameRegex.py (.*)_EXPD.xml 10\1_IMP.xml <folder>
+#   py BatchRenameRegex.py .*_EXPD.xml 10\g<0> <folder>
+# Remark:
+#   \1,\2 are the matched parenthesized subgroup
+#   \g<0> is the entire match
+
+import sys
+import os
+import re
+  
+if (len(sys.argv)<=3):
+    print ("Usage:")
+    print ("    py {} <pattern> <repl> <folder>".format(os.path.basename(__file__)))
+    sys.exit()
+
+strRegex = sys.argv[1]
+strRepl = sys.argv[2]
+strFolderIn = sys.argv[3]
+
+p = re.compile(strRegex)
+
+for count, filenameOld in enumerate(os.listdir(strFolderIn)):
+    filenameNew = p.sub(strRepl, filenameOld)
+    src = strFolderIn + '\\' + filenameOld 
+    dst = strFolderIn + '\\' + filenameNew 
+    os.rename(src, dst) 
+
