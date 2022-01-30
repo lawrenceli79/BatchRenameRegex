f9e2786f80f6ca3bb8c2ab9ca6d7e7acc048fd23
Lawrence Li <lawrenceli79@gmail.com>
2022-01-30 15:23:13 +0800

    v

--- BatchRenameRegex.py
+++ BatchRenameRegex.py
@@ -21,11 +21,19 @@ strRegex = sys.argv[1]
 strRepl = sys.argv[2]
 strFolderIn = sys.argv[3]
 
+print("strRegex=" + strRegex)
+print("strRepl=" + strRepl)
+print("strFolderIn=" + strFolderIn)
+
 p = re.compile(strRegex)
 
-for count, filenameOld in enumerate(os.listdir(strFolderIn)):
+count = 0
+for i, filenameOld in enumerate(os.listdir(strFolderIn)):
     filenameNew = p.sub(strRepl, filenameOld)
-    src = strFolderIn + '\\' + filenameOld 
-    dst = strFolderIn + '\\' + filenameNew 
-    os.rename(src, dst) 
+    if(filenameOld != filenameNew):
+        src = strFolderIn + '\\' + filenameOld 
+        dst = strFolderIn + '\\' + filenameNew 
+        os.rename(src, dst) 
+        count = count + 1
 
+print("Renamed Count={}".format(count))
